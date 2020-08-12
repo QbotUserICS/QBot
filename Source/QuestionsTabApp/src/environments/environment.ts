733ea6b6eef25fd202db87797719e042225b260f
QbotUserICS <69473325+QbotUserICS@users.noreply.github.com>
2020-08-12 08:40:48 -0700

    Update environment.ts

--- Source/QuestionsTabApp/src/environments/environment.ts
+++ Source/QuestionsTabApp/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qboticsukedu.azurewebsites.net/api/Request/",
-    selfUrl: "https://qboticsukedu-questions.azurewebsites.net",
+    apiBaseUrl: "https://qboticsukedufour.azurewebsites.net/api/Request/",
+    selfUrl: "https://qboticsukedufour-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
