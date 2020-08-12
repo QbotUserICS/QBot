f73e8554ea404cf464c62a9cc05a07466ad6465c
QbotUserICS <69473325+QbotUserICS@users.noreply.github.com>
2020-08-12 08:40:07 -0700

    Update environment.ts

--- Source/DashboardTabApp/src/environments/environment.ts
+++ Source/DashboardTabApp/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qboticsukedu.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://qboticsukedufour.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
